51ee28619059c81662c57d14fcd703013cced124
Berkay Emre Aslan <emreaslan1177@gmail.com>
2025-07-07 17:58:43 +0300

    Setup Fonts and Assets

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-        <Text className="my-10">Welcome to the ReState</Text>
+        <Text className="font-bold my-10 font-rubik text-3xl">Welcome to the ReState</Text>
         <Link href="/sign-in">Sign In</Link>
         <Link href="/explore">Explore</Link>
         <Link href="/profile">Profile</Link>
